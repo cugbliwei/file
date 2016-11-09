966bcd5020e40011bf2b5cec6207339cd524a0da
weili105 <weili105@creditease.cn>
2016-11-09 20:57:09 +0800

    add health check

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 	_ "net/http/pprof"
 )
 
+func HandleHealth(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "ok")
+}
+
 func main() {
 	port := flag.String("port", "8001", "port number")
 	flag.Parse()
+
+	http.HandleFunc("/health", HandleHealth)
 	http.Handle("/temp/", http.StripPrefix("/site/", http.FileServer(http.Dir("./site"))))
 	l, e := net.Listen("tcp", ":"+*port)
 	if e != nil {
